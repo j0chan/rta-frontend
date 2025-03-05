90d20aa01c4a40b15b37c1a49929f809c641554b
3405000 <jaeeunnooh@gmail.com>
2025-03-05 13:46:09 +0900

    refactor: review-list selector 이름 수정

--- src/app/pages/reviews/review-list/review-list.component.ts
+++ src/app/pages/reviews/review-list/review-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'app-reviews',
+  selector: 'app-review-list',
   templateUrl: './review-list.component.html',
   styleUrls: ['./review-list.component.scss'],
   standalone: false,
