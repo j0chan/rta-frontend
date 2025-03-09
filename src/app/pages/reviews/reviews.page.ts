e4d1e3f8486e4e1e044397b6da74a14771de34d4
3405000 <jaeeunnooh@gmail.com>
2025-03-09 22:23:20 +0900

    refactor: review 관련 코드 정리

--- src/app/pages/reviews/reviews.page.ts
+++ src/app/pages/reviews/reviews.page.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core'
-import { ReviewListComponent } from 'src/app/pages/reviews/review-list/review-list.component'
 
 @Component({
   selector: 'app-reviews-page',
   templateUrl: './reviews.page.html',
   styleUrls: ['./reviews.page.scss'],
   standalone: false,
-  // imports: [ReviewsComponent]
 })
 export class ReviewsPage implements OnInit {
 
