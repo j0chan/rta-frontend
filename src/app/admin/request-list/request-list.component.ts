f344999ad47eff2d3a3663e89b0ee1b4e7a824c1
3405000 <jaeeunnooh@gmail.com>
2025-03-10 17:35:37 +0900

    fix: fix path errors

---
+++ src/app/admin/request-list/request-list.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core'
+import { RequestPage } from 'src/app/model/common/request-page.enum'
+import { ReadManagerRequest } from 'src/app/model/manager-requests/read-manager-request.interface'
+import { ManagerRequestsService } from 'src/app/services/manager-requests.service'
+import { StoreRequestsService } from 'src/app/services/store-requests.service'
+
+@Component({
+  selector: 'app-request-list',
+  templateUrl: './request-list.component.html',
+  styleUrls: ['./request-list.component.scss'],
+  standalone: false,
+})
+export class RequestListComponent implements OnInit {
+  requestPage: RequestPage = RequestPage.MANAGER_REQUEST
+  requests: ReadManagerRequest[] | ReadManagerRequest[] = []
+
+  constructor(
+    private managerRequestsService: ManagerRequestsService,
+    private storeRequestsService: StoreRequestsService,
+  ) { }
+
+  ngOnInit() {
+    switch (this.requestPage) {
+      case RequestPage.MANAGER_REQUEST:
+        this.loadManagerRequests()
+        break
+      case RequestPage.STORE_REQUEST:
+        this.loadStoreRequests()
+        break
+      default:
+        console.warn("Unknown requestPage: ", this.requestPage)
+    }
+  }
+
+  loadManagerRequests() {
+    this.managerRequestsService.readAllManagerRequests().subscribe({
+      next: response => {
+        if (response.success) {
+          this.requests = response.data || []
+        } else {
+          console.error(response.message)
+        }
+      },
+      error: err => {
+        console.error('Error fetching manager requests: ', err)
+      },
+      complete: () => {
+        console.log('Fetching manager requests completed')
+      }
+    })
+  }
+
+  loadStoreRequests() {
+    this.storeRequestsService.readAllStoreRequests().subscribe({
+      next: response => {
+        if (response.success) {
+          this.requests = response.data || []
+        } else {
+          console.error(response.message)
+        }
+      },
+      error: err => {
+        console.error('Error fetching store requests: ', err)
+      },
+      complete: () => {
+        console.log('Fetching store requests completed')
+      }
+    })
+  }
+
+  getPageTitle(): string {
+    return (this.requestPage == RequestPage.MANAGER_REQUEST) ? "Manager Requests" : "Store Requests"
+  }
+}
