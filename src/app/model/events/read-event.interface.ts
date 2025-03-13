eb88e44fc7b95ba3c2c64746165f6673bb58c36e
Youngchan <wdc4512@naver.com>
2025-03-13 21:21:35 +0900

    feat: ReadEvent관련 enum, interface 추가

---
+++ src/app/model/events/read-event.interface.ts
@@ -0,0 +1,11 @@
+import { EventStatus } from "../common/event-status.enum"
+
+export interface ReadEvent {
+    event_id: number
+    title: string
+    description: string
+    start_date: Date
+    end_date: Date
+    event_status: EventStatus
+    created_at: Date
+}
