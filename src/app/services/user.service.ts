efd9ec22efc51f6539acaa62e9562de3092fb775
3405000 <jaeeunnooh@gmail.com>
2025-03-15 12:20:25 +0900

    feat: usersService 생성

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ApiResponseDTO } from '../model/common/api-response.interface'
+import { ReadReview } from '../model/reviews/read-review.interface'
+import { HttpClient } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:3000/api/users/'
+
+  readReviewById(user_id: number): Observable<ApiResponseDTO<ReadReview>> {
+    return this.http.get<ApiResponseDTO<ReadReview>>(`${this.apiUrl}${user_id}`)
+  }
+}
