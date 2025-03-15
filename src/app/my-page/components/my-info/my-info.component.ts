19e78793db675e33f02f8f7b13ed19b2e20845fd
3405000 <jaeeunnooh@gmail.com>
2025-03-15 16:49:34 +0900

    feat: my page 구현

--- src/app/my-page/components/my-info/my-info.component.ts
+++ src/app/my-page/components/my-info/my-info.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { ReadUser } from 'src/app/model/users/read-user.interface'
+import { UserRole } from 'src/app/model/users/user-role.enum'
 
 @Component({
   selector: 'app-my-info',
   templateUrl: './my-info.component.html',
   styleUrls: ['./my-info.component.scss'],
+  standalone: false,
 })
-export class MyInfoComponent  implements OnInit {
+export class MyInfoComponent implements OnInit {
+  user!: ReadUser
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.user = {
+      user_id: 1,
+      email: 'example@example.com',
+      nickname: '재은이',
+      phone_number: '010-1234-4567',
+      role: UserRole.USER,
+      created_at: new Date()
+    }
+  }
 
 }
