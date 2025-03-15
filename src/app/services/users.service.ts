ae8fec927bcc3e76adcef8fa9445b8c0ad381247
3405000 <jaeeunnooh@gmail.com>
2025-03-15 12:23:28 +0900

    refactor: user -> users 이름 변경

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UsersService {
   constructor(private http: HttpClient) { }
   private apiUrl = 'http://localhost:3000/api/users/'
 
