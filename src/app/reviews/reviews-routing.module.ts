5b0550781cb977875ba1d1ff919f068ae1835b1a
3405000 <jaeeunnooh@gmail.com>
2025-03-15 12:02:00 +0900

    refactor: delete reviews page

--- src/app/reviews/reviews-routing.module.ts
+++ src/app/reviews/reviews-routing.module.ts
@@ -1,14 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { ReviewsPage } from './reviews.page';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ReviewsPage
-  }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
