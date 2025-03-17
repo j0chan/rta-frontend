0db710a601078b453ad8e5d30a28f8f62c4c8fe9
3405000 <jaeeunnooh@gmail.com>
2025-03-17 11:43:20 +0900

    feat: usersService readMyManagerRequests 생성 및 적용

--- src/app/manager/components/manager-request-list/manager-request-list.component.ts
+++ src/app/manager/components/manager-request-list/manager-request-list.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core'
+import { ReadManagerRequest } from 'src/app/model/manager-requests/read-manager-request.interface'
+import { UsersService } from 'src/app/services/users.service'
 
 @Component({
   selector: 'app-manager-request-list',
   templateUrl: './manager-request-list.component.html',
 })
-export class ManagerRequestListComponent  implements OnInit {
+export class ManagerRequestListComponent implements OnInit {
+  requests: ReadManagerRequest[] = []
 
-  constructor() { }
+  // 임시 사용자 아이디
+  user_id: number = 1
 
-  ngOnInit() {}
+  constructor(
+    private usersService: UsersService,
+  ) { }
 
+  ngOnInit() {
+    this.loadMyManagerRequests()
+  }
+
+  loadMyManagerRequests() {
+    this.usersService.readMyManagerRequests(this.user_id).subscribe({
+      next: response => {
+        if (response.success) {
+          this.requests = response.data || []
+        } else {
+          console.error(response.message)
+        }
+      },
+      error: err => {
+        console.error('Error fetching manager requests: ', err)
+      },
+      complete: () => {
+        console.log('Fetching manager requests completed')
+      }
+    })
+  }
 }
