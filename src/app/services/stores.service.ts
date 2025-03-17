1946407521271778244947bdb679e8ddbea19c85
Youngchan <wdc4512@naver.com>
2025-03-17 16:47:23 +0900

    refactor: apiUrl 통일

--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -11,27 +11,27 @@ import { ReadReview } from '../model/reviews/read-review.interface'
     providedIn: 'root'
 })
 export class StoresService {
-    private apiUrl = 'http://localhost:3000/api/stores'
+    private apiUrl = 'http://localhost:3000/api/stores/'
 
     constructor(private http: HttpClient) { }
 
     getStoreById(id: number): Observable<ApiResponseDTO<ReadStore>> {
-        return this.http.get<ApiResponseDTO<ReadStore>>(`${this.apiUrl}/${id}`)
+        return this.http.get<ApiResponseDTO<ReadStore>>(`${this.apiUrl}${id}`)
     }
 
     getLastestEventByStoreId(store_id: number): Observable<ApiResponseDTO<ReadEvent>> {
-        return this.http.get<ApiResponseDTO<ReadEvent>>(`${this.apiUrl}/${store_id}/events/latest`)
+        return this.http.get<ApiResponseDTO<ReadEvent>>(`${this.apiUrl}${store_id}/events/latest`)
     }
 
     getMenusByStoreId(store_id: number): Observable<ApiResponseDTO<ReadMenu[]>> {
-        return this.http.get<ApiResponseDTO<ReadMenu[]>>(`${this.apiUrl}/${store_id}/menus`)
+        return this.http.get<ApiResponseDTO<ReadMenu[]>>(`${this.apiUrl}${store_id}/menus`)
     }
 
     getReviewsByStoreId(store_id: number): Observable<ApiResponseDTO<ReadReview[]>> {
-        return this.http.get<ApiResponseDTO<ReadReview[]>>(`${this.apiUrl}/${store_id}/reviews`)
+        return this.http.get<ApiResponseDTO<ReadReview[]>>(`${this.apiUrl}${store_id}/reviews`)
     }
 
     getAllEventsByStore(store_id: number): Observable<ApiResponseDTO<ReadEvent[]>> {
-        return this.http.get<ApiResponseDTO<ReadEvent[]>>(`${this.apiUrl}/${store_id}/events`)
+        return this.http.get<ApiResponseDTO<ReadEvent[]>>(`${this.apiUrl}${store_id}/events`)
     }
 }
