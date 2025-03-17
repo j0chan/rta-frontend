26ff1305f6c85a81f9c89a8bbac161a70010f5df
3405000 <jaeeunnooh@gmail.com>
2025-03-17 10:58:14 +0900

    feat: manager request component 생성

---
+++ src/app/manager/components/manager-request/manager-request.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-manager-request',
+  templateUrl: './manager-request.component.html',
+})
+export class ManagerRequestComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular'
 
-import { ManagerRoutingModule } from './manager-routing.module';
+import { ManagerRoutingModule } from './manager-routing.module'
 
 import { ManagerPage } from './pages/manager/manager.page'
-import { MyManagerRequestsPage } from './pages/my-manager-request/my-manager-requests.page';
+import { MyManagerRequestsPage } from './pages/my-manager-request/my-manager-requests.page'
+import { ManagerRequestComponent } from './components/manager-request/manager-request.component'
 
 @NgModule({
   imports: [
@@ -19,6 +20,7 @@ import { MyManagerRequestsPage } from './pages/my-manager-request/my-manager-req
   declarations: [
     ManagerPage,
     MyManagerRequestsPage,
+    ManagerRequestComponent,
   ]
 })
 export class ManagerModule {}
