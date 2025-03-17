9745d8605d119928bb6f64810f372aff2af94c4d
3405000 <jaeeunnooh@gmail.com>
2025-03-17 11:09:51 +0900

    feat: manager request list component 생성

---
+++ src/app/manager/components/manager-request-list/manager-request-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-manager-request-list',
+  templateUrl: './manager-request-list.component.html',
+})
+export class ManagerRequestListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -9,6 +9,7 @@ import { ManagerRoutingModule } from './manager-routing.module'
 import { ManagerPage } from './pages/manager/manager.page'
 import { MyManagerRequestsPage } from './pages/my-manager-request/my-manager-requests.page'
 import { ManagerRequestComponent } from './components/manager-request/manager-request.component'
+import { ManagerRequestListComponent } from './components/manager-request-list/manager-request-list.component'
 
 @NgModule({
   imports: [
@@ -21,6 +22,7 @@ import { ManagerRequestComponent } from './components/manager-request/manager-re
     ManagerPage,
     MyManagerRequestsPage,
     ManagerRequestComponent,
+    ManagerRequestListComponent,
   ]
 })
 export class ManagerModule {}
