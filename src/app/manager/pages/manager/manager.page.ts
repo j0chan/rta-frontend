b72c70e3cb313eca55e0be7b0e170301c2295863
3405000 <jaeeunnooh@gmail.com>
2025-03-17 12:51:58 +0900

    feat: manager home page 구현

--- src/app/manager/pages/manager/manager.page.ts
+++ src/app/manager/pages/manager/manager.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-manager',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core'
 })
 export class ManagerPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
   }
 
+  goMyManagerRequestsPage() {
+    console.log('go my manager requests page')
+    this.router.navigate(['my-manager-requests'], { relativeTo: this.route })
+  }
 }
