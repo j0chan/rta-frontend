13ff5a185413b34b189e2f97e6758b0400c700cd
Youngchan <wdc4512@naver.com>
2025-03-20 19:45:28 +0900

    feat: 로그인 시 발생하는 JWT 토큰 저장

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { CreateUserDTO } from '../model/auth/create-user.interface'
-import { Observable } from 'rxjs'
+import { Observable, tap } from 'rxjs'
 import { ApiResponseDTO } from '../model/common/api-response.interface'
 import { SignInDTO } from '../model/auth/singin.interface'
 
@@ -17,7 +17,17 @@ export class AuthService {
     return this.http.post<ApiResponseDTO<void>>(`${this.apiUrl}signup`, createUserDTO)
   }
 
-  signIn(signInDTO: SignInDTO): Observable<ApiResponseDTO<void>> {
-    return this.http.post<ApiResponseDTO<void>>(`${this.apiUrl}signin`, signInDTO)
+  signIn(signInDTO: SignInDTO): Observable<ApiResponseDTO<{ accessToken: string }>> {
+    return this.http.post<ApiResponseDTO<{ accessToken: string }>>(`${this.apiUrl}signin`, signInDTO).pipe(
+      tap((response) => {
+        if (response.success && response.data?.accessToken) {
+          localStorage.setItem('accessToken', response.data.accessToken)
+        }
+      })
+    )
+  }
+
+  signOut(): void {
+    localStorage.removeItem('accessToken') // 로그아웃 시 토큰 삭제
   }
 }
