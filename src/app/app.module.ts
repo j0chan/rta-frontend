fe8c8c0f573a0c22e53444e5e452cf6af33b0297
Youngchan <wdc4512@naver.com>
2025-03-20 11:46:12 +0900

    refactor: HttpClientModule 사용 중지, provideHttpClient() 사용으로 대체

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouteReuseStrategy } from '@angular/router'
@@ -18,13 +18,17 @@ import { SharedModule } from './shared/module/shared.module'
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule,
     ReviewsModule,
     AdminModule,
     FormsModule,
     SharedModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy 
+    },
+    provideHttpClient(),
+    ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
