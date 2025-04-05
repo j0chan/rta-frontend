de94bca9bcbedaab9133a421103cc210dcfd81b1
3405000 <jaeeunnooh@gmail.com>
2025-04-05 14:51:20 +0900

    feat: 가게 키워드로 검색 기능 구현

--- src/app/search/pages/search/search.page.ts
+++ src/app/search/pages/search/search.page.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core'
+import { ApiResponseDTO } from 'src/app/shared/model/common/api-response.interface'
+import { ReadStore } from 'src/app/shared/model/stores/read-store.interface'
+import { SearchService } from 'src/app/shared/services/search.service'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
-  styleUrls: ['./search.page.scss'],
+  standalone: false,
 })
 export class SearchPage implements OnInit {
-
-  constructor() { }
+  keyword: string = ''
+  searchedStores: ReadStore[] = []
+  constructor(private searchService: SearchService) { }
 
   ngOnInit() {
   }
 
+  searchByKeyword() {
+    console.log(`search by keyword ${this.keyword}`)
+    
+    this.searchService.searchStoresByKeyword(this.keyword).subscribe({
+      next: response => {
+        if (response.success) {
+          this.searchedStores = response.data ?? []
+        }
+      },
+      error: (err) => {
+        console.error('Failed to Retriving Events', err)
+      }
+    })
+  }
 }
