375f881255fd88986054a5b21ee782b6c6156fb5
CHJ <huihee@g.hongik.ac.kr>
2025-04-12 21:55:27 +0900

    feat: X 버튼 초기화 기능 추가

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -10,8 +10,14 @@ export class SearchBarComponent {
     searchQuery: string = ''
 
     @Output() searchEvent = new EventEmitter<string>()
+    @Output() clearEvent = new EventEmitter<void>() // X 버튼 클릭 시 알림용
 
     search() {
         this.searchEvent.emit(this.searchQuery) // 입력된 검색어를 부모 컴포넌트로 전달
+    }
+
+    clearSearch() {
+        this.searchQuery = ''
+        this.clearEvent.emit()
     }
 }
