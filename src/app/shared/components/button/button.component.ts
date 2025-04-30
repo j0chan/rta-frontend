a16a167ef477b35d8b9a94769d85f4ad271a426c
3405000 <jaeeunnooh@gmail.com>
2025-04-30 23:25:26 +0900

    design: button component 변수 추가

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,15 +8,27 @@ import { Component, Input, OnInit } from '@angular/core'
 })
 export class ButtonComponent  implements OnInit {
   // 버튼에 보이는 이름
-  @Input() label = "Button"
-  // 버튼의 종류(디자인)
+  @Input() label = "Label"
+  // 버튼 색상
   /**
-   * primary : 기본 버튼 (예: 리뷰 작성하기)
-   * secondary : 하위 페이지 접속과 같은 비주류 버튼 (예: 더보기)
-   * small : 기본 버튼의 작은 크기
-   * primary-full : 좌우 여백 없이 꽉찬 기본 버튼
+   * filled : (기본) 파란 배경 버튼
+   * tinted : 하늘색 배경 + 파란 글씨 버튼
+   * tinted-bordered : 하늘색 배경, 파란 테두리 + 파란 글씨 버튼
+   * tinted-gray : 회색 배경, 회색 테두리 + 회색 글씨 버튼
+   * bordered-gray : 회색 테두리 버튼
+   * borderless : 배경없는 회색 비주류 버튼
+   * disabled : 회색 배경 + 회색 글씨 버튼
    */
-  @Input() variant: 'primary' | 'secondary' | 'small' | 'primary-full' = 'secondary'
+  @Input() variant: 'filled' | 'tinted' | 'tinted-bordered' | 'tinted-gray' | 'bordered-gray' | 'borderless' = 'filled'
+  // 버튼 및 텍스트 크기
+  /**
+   * full : 좌우 여백을 다 채우는 버튼 (16px)
+   * fit : 텍스트 사이즈에 맞게 (16px)
+   * fit-small : 텍스트 사이즈에 맞게, 작은 버튼 (12px)
+   * small : 작은 버튼 (12px)
+   * symbol : 심볼이 포함된 버튼 (14px)
+   */
+  @Input() size: 'full' | 'fit' | 'fit-small' | 'small' | 'symbol' = 'full'
   @Input() disabled = false
 
   constructor() { }
