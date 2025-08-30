23c042d3e532e995a95097e6c4ea203f2dc45f4b
CHJ <huihee@g.hongik.ac.kr>
2025-08-31 01:44:25 +0900

    feat: 관리자 화면 비고 조회 기능 수정 및 승인 확인 모달, 화면 reload 적용

--- src/app/admin/components/manager-request/manager-request.component.ts
+++ src/app/admin/components/manager-request/manager-request.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 import { RequestStatus } from 'src/app/shared/model/common/request-status.enum';
 import { ReadManagerRequest } from 'src/app/shared/model/manager-requests/read-manager-request.interface';
 import { UpdateManagerRequest } from 'src/app/shared/model/manager-requests/update-manager-request.interface';
@@ -16,12 +17,15 @@ export class ManagerRequestComponent implements OnInit {
   request_id: number | undefined;
   status: RequestStatus | undefined;
   remark: string = '';
+  selectedStatus: RequestStatus | null = null;
 
   public RequestStatus = RequestStatus;
 
   constructor(
     private managerRequestsService: ManagerRequestsService,
     private router: Router,
+    private toastController: ToastController,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
@@ -30,25 +34,52 @@ export class ManagerRequestComponent implements OnInit {
     }
   }
 
-  selectedStatus: RequestStatus | null = null;
-
   toggleStatus(status: RequestStatus) {
     this.selectedStatus = status;
   }
 
+  async confirmAndUpdate() {
+    if (!this.request_id || !this.selectedStatus) return;
+
+    // 알림창 띄우기
+    const alert = await this.alertController.create({
+      header: '확인',
+      message: '승인하시겠습니까?',
+      buttons: [
+        {
+          text: '취소',
+          role: 'cancel',
+        },
+        {
+          text: '확인',
+          handler: async () => {
+            await this.updateManagerRequest();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
   async updateManagerRequest() {
-    if (!this.request_id || !this.status) return;
+    if (!this.request_id || !this.selectedStatus) return;
 
     const updateData: UpdateManagerRequest = {
-      status: this.status,
-      remark: this.remark
+      status: this.selectedStatus,
+      remark: this.request.remark
     };
 
     this.managerRequestsService.updateManagerRequest(this.request_id, updateData).subscribe({
-      next: response => {
-        if (response.success) {
+      next: async (response) => {
+        if (this.selectedStatus !== null) {
+          //this.request.status = this.selectedStatus; // 토스트 사용 시 UI 갱신
+          //this.request.remark = this.remark; // 토스트 사용 시 UI 갱신
+          //await this.showToast('저장되었습니다'); // 성공 알림
+          window.location.reload();
           console.log('confirm manager request successfully');
         } else {
+          await this.showToast('저장에 실패했습니다');
           console.error('confirm manager request failed: ', response.message);
         }
       },
@@ -60,5 +91,15 @@ export class ManagerRequestComponent implements OnInit {
       }
     });
   }
-  
+
+  // 토스트 표시 메소드
+  private async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000, // 2초간 표시
+      position: 'bottom',
+      color: 'warning' // primary, secondary, tertiary, success, warning, danger, light, medium, dark
+    });
+    toast.present();
+  }
 }
